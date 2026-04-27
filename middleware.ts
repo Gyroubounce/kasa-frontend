e4552251289a0de4f4fd5923a944b99cf7a0244a
Lemonnier Serge <lidry@hotmail.fr>
2026-04-27 20:12:32 +0200

    Logs Middleware

--- middleware.ts
+++ middleware.ts
@@ -3,17 +3,25 @@ import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
+  const path = req.nextUrl.pathname;
+
+  console.log("🟦 [MIDDLEWARE] PATH:", path);
+  console.log("🟦 [MIDDLEWARE] TOKEN:", token);
 
   const protectedRoutes = ["/favorites", "/add-property", "/messaging"];
 
   const isProtected = protectedRoutes.some((route) =>
-    req.nextUrl.pathname.startsWith(route)
+    path.startsWith(route)
   );
 
+  console.log("🟦 [MIDDLEWARE] isProtected:", isProtected);
+
   if (isProtected && !token) {
+    console.log("❌ [MIDDLEWARE] NO TOKEN → REDIRECT /login");
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
+  console.log("🟩 [MIDDLEWARE] ACCESS GRANTED → NEXT()");
   return NextResponse.next();
 }
 
